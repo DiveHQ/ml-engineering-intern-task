a46b533835577c13fd5a4dd744ebbb54d772d971
Debal Husain Abbas <debal@Debals-MacBook-Air.local>
2023-05-08 21:53:03 +0530

    Added Pipeline Code

--- pipeline.py
+++ pipeline.py
@@ -6,42 +6,44 @@ from transformers import AutoTokenizer, BartForConditionalGeneration
 import json
 from data.utils import get_completion
 
-with open('data/datagen_vars.yml','r',encoding='UTF-8') as f:
-        data = yaml.load(f, Loader=SafeLoader)
-
-
-# ======================================= RealTime Dialogue generation Code =======================================
-df = pd.read_csv('data/datasets/inference_scenario.csv')
-
 tokenizer = AutoTokenizer.from_pretrained("Debal/action-item-generator")
 
 model = BartForConditionalGeneration.from_pretrained("Debal/action-item-generator")
 
-exp = data['example'].split('Action Items:\n')[0]
-example = random.choice(list(df.values))
-prompt_dialogue = f"""
-{exp}
 
-Scenario: {example[0]}
-Participants : {example[1]}
-Turns: {example[2]}
-"""
 
-response = get_completion(prompt_dialogue)
+## ======================================= RealTime Dialogue generation Code =======================================
+# df = pd.read_csv('data/datasets/inference_scenario.csv')
 
-dialogue = response.split('\n')
-dialogue_list = [diag.split('||')[-2:] for diag in dialogue]
-dialogue_list = [":".join(x) for x in dialogue_list]
-inp = '\n'.join(dialogue_list)
+# with open('data/datagen_vars.yml','r',encoding='UTF-8') as f:
+#         data = yaml.load(f, Loader=SafeLoader)
 
-# ======================================= Static Dialogue Fetch =======================================
-# with open('data/datasets/inference_data.json','r') as f:
-#     data = json.load(f)
 
-# inp = random.choice(data)
+# exp = data['example'].split('Action Items:\n')[0]
+# example = random.choice(list(df.values))
+# prompt_dialogue = f"""
+# {exp}
+
+# Scenario: {example[0]}
+# Participants : {example[1]}
+# Turns: {example[2]}
+# """
+
+# response = get_completion(prompt_dialogue)
+
+# dialogue = response.split('\n')
+# dialogue_list = [diag.split('||')[-2:] for diag in dialogue]
+# dialogue_list = [":".join(x) for x in dialogue_list]
+# inp = '\n'.join(dialogue_list)
+
+## ======================================= Static Dialogue Fetch =======================================
+with open('data/datasets/inference_data.json','r') as f:
+    data = json.load(f)
+
+inp = random.choice(data)
 
 print("Input\n", inp)
-exit()
+
 inputs = tokenizer([inp],return_tensors='pt')
 summary_ids = model.generate(inputs["input_ids"],max_length=256)
 action_items = tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
